b69ac81d69500395d214b349c56be825772c69c6
metalconker <2914720+metalconker@users.noreply.github.com>
2020-08-08 23:00:16 -0400

    interessant

--- individual.py
+++ individual.py
@@ -1,11 +1,18 @@
+import os
+import sys
 import urllib
 import urllib.request
 import requests
 import re
 from bs4 import BeautifulSoup
+from requests_html import HTMLSession
+import subprocess
+
+exerciseWebsiteName = "ContraLateralSuperman"
+exerciseFullName = ""
 
 # URL Setup
-url = "https://exrx.net/WeightExercises/ErectorSpinae/TBStraightLegDeadlift"
+url = "https://exrx.net/WeightExercises/ErectorSpinae/" + exerciseWebsiteName
 req = urllib.request.Request(url, headers={
                              'User-Agent': "Mozilla/5.0 (X11; Linux x86_64; rv:86.0) Gecko/20100101 Firefox/86.0"})
 try:
@@ -14,21 +21,44 @@ except urllib.error.URLError as e:
     print(e.reason)
 soup = BeautifulSoup(page, "lxml")
 
-##############################
-# headers = {
-#     'Host': 'exrx.glorb.com',
-#     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:79.0) Gecko/20100101 Firefox/79.0',
-#     'Accept': 'video/webm,video/ogg,video/*;q=0.9,application/ogg;q=0.7,audio/*;q=0.6,*/*;q=0.5',
-#     'Accept-Language': 'en-US,en;q=0.5',
-#     'Range': 'bytes=0-',
-#     'DNT': '1',
-#     'Connection': 'keep-alive',
-#     'Referer': 'https://exrx.net/'}
+# Finds the name of the exercise
+content = soup.find(
+    'div', {"class": "fruitful-page-title fruitfull-title-padding"})
+article = 'Exercise: '
+for i in content.findAll('h1'):
+    article = article + i.text
+    exerciseFullName = i.text
 
-# videoURL = 'https://exrx.glorb.com/api/video/10601'
+# File
+subdirectory = exerciseFullName
+if not os.path.exists(exerciseFullName):
+    os.makedirs(exerciseFullName)
+file = open(os.path.join(subdirectory, 'details.json'), 'w')
+file.write(article + '\n')
+
+##############################
+subprocess.run(["youtube-dl", "-v", "https://player.vimeo.com/video/156692562","--referer", url, "-o", '/'+exerciseFullName+"/video.%(ext)s"])
+# session = HTMLSession()
+# r = session.get(url)
+# r.html.render()
+
+# headers = {
+# 'Host': 'player.vimeo.com',
+# 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0',
+# 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+# 'Accept-Language': 'en-US,en;q=0.5',
+# 'Accept-Encoding': 'gzip, deflate, br',
+# 'DNT': '1',
+# 'Connection': 'keep-alive',
+# 'Referer': 'https://exrx.net/',
+# 'Upgrade-Insecure-Requests': '1',
+# 'Pragma': 'no-cache',
+# 'Cache-Control': 'no-cache'}
+
+# videoURL = 'https://player.vimeo.com/video/157104877?muted=1&autoplay=1&loop=1&title=0&byline=0&portrait=0'
 # r = requests.get(videoURL, headers)
-# print(r.headers)
-# open("video.mp4", 'wb').write(r.content)
+# print(r.status_code)
+#open("video.mp4", 'wb').write(r.content)
 
 
 #videoreq = urllib.request.Request(videoURL, headers)
@@ -47,17 +77,6 @@ soup = BeautifulSoup(page, "lxml")
 
 
 ################################
-
-# File
-file = open('scraped_text.txt', 'w')
-
-# Finds the name of the exercise
-content = soup.find(
-    'div', {"class": "fruitful-page-title fruitfull-title-padding"})
-article = 'Exercise: '
-for i in content.findAll('h1'):
-    article = article + i.text
-file.write(article + '\n')
 
 leftDiv = soup.findAll('div', {"class": "col-sm-6"})[0]
 rightDiv = soup.findAll('div', {"class": "col-sm-6"})[1]
@@ -123,7 +142,7 @@ if (rightSideStart):
 muscleGroupNames = rightDiv.findAll('p')
 muscleNames = rightDiv.findAll('ul')
 for i in range(1, len(muscleGroupNames)):
-    file.write(muscleGroupNames[i].text + ': ')
+    file.write(muscleGroupNames[i].text.rstrip('\n') + ': ')
     currentMuscles = muscleNames[i-1].findAll('li')
     muscles = ""
     for muscle in currentMuscles:
---
+++ videoscraper.py
@@ -0,0 +1,15 @@
+import urllib
+import urllib.request
+import requests
+import re
+from bs4 import BeautifulSoup
+from requests_html import HTMLSession
+
+# URL Setup
+URL = 'https://exrx.net/WeightExercises/ErectorSpinae/CBStraightLegDeadlift'
+session = HTMLSession()
+r = session.get(URL, headers={'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0"})
+r.html.render()
+print(r.headers)
+print(r.content)
+	/video/157017805
